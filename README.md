a656b9a4d5fb84ad8fddc588ad32e71629c39848
Dj Walker-Morgan <codepope@gmail.com>
2018-03-15 10:44:07 +0000

    First commit with code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # stunredis
+
 No-configuration connections for redis-cli to Redis TLS services 
+
+## Use
+
+To run stunredis.sh:
+
+* Download the files.
+* `chmod u+x stunredis.sh` to make it executable.
+* Get a connection string for your Redis database.
+* Run `./stunredis.sh <connection string>`
+
+## Notes on lechain.pem
+
+The lechain.pem file is a sample of the verification chain for Lets Encrypt. Do not use for production if you are concerned about correctness.
+
+You can be create your own version of lechain.pem by downloading and combining the contents of the [Let's Encrypt X3 Cross-signed PEM file](https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt) and the [IdenTrust Root for X3](https://www.identrust.com/certificates/trustid/root-download-x3.html). (The latter link's content will need to be wrapped in the same -----BEGIN CERTIFICATE-----/-----END CERTIFICATE----- lines that the first links content is wrapped in). Consult lechain.pem for an example of how it should look.
+
+For simplicity, it is located in the same directory as the stunredis.sh script.
+
+
+
---
+++ stunredis.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Stunredis.sh
+#
+# Copyright 2018 IBM Corp.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+DATABASE_URL=$1
+LOCALPORT=${2:-6830}
+
+# This is the location of the validation chain file
+lechain=./lechain.pem
+
+# URL parsing based on https://stackoverflow.com/a/17287984
+# extract the protocol
+proto="`echo $DATABASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
+# remove the protocol
+url=`echo $DATABASE_URL | sed -e s,$proto,,g`
+# extract the user and password (if any)
+userpass="`echo $url | grep @ | cut -d@ -f1`"
+pass=`echo $userpass | grep : | cut -d: -f2`
+if [ -n "$pass" ]; then
+    user=`echo $userpass | grep : | cut -d: -f1`
+else
+    user=$userpass
+fi
+hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
+port=`echo $hostport | grep : | cut -d: -f2`
+if [ -n "$port" ]; then
+    host=`echo $hostport | grep : | cut -d: -f1`
+else
+    host=$hostport
+fi
+
+# Now we create our configuration file as a variable
+stunnelconf=""
+stunnelconf+=$"foreground=yes\n" 
+stunnelconf+=$"[redis-cli]\n"
+stunnelconf+=$"client=yes\n"
+stunnelconf+=$"accept=127.0.0.1:$LOCALPORT\n"
+stunnelconf+=$"verifyChain=yes\n"
+
+stunnelconf+=$"checkHost=$host\n"
+stunnelconf+=$"CAfile=$lechain\n"
+stunnelconf+=$"connect=$hostport\n"
+
+# We expand that out in echo and feed the result to stunnel
+# which is set to take its configuration from a file descriptor
+# in this case, 0, stdin.
+
+echo -e $stunnelconf | stunnel -fd 0 &
+
+# Grab the pid
+stunnelpid=$!
+# Sleep a moment to let the connection establish
+sleep 1 
+# Now call redis-cli for the user to interact with
+redis-cli -p $LOCALPORT -a ${pass}
+# Once they leave that, kill the stunnel
+kill $stunnelpid
+
+
